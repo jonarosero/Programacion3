bc7a23a035875442a92f809c1212839e0f3cdd39
jonarosero <jars9605@hotmail.com>
2018-06-10 18:21:02 -0500

    data base mysql with java, update, delete and select

---
+++ U9_extraclase/src/modelo/Producto.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Producto {
+    private String codigo;
+    private String nombre;
+    private double precio;
+    private int cantidad;
+    
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+    
+    
+    
+}
